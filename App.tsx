60341262159ff9f298e9d2dcd2476829e59b5180
Jorge Perez <jorgeperez@MacBook-Air-de-Jorge.local>
2022-05-08 01:00:40 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ContadorScreens from './src/screens/ContadorScreens'
+
+const App = () => {
+  return (
+    <ContadorScreens/>
+  )
+}
+
+export default App
---
+++ src/screens/ContadorScreens.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import { Text, View, Button, TouchableOpacity, StyleSheet } from 'react-native'
+import { Fab } from '../components/Fab';
+
+const ContadorScreens = () => {
+
+    const [contador, setContador] = useState(10);
+
+    return (
+        <View style={ styles.container }>
+            <Text style={ styles.title }>
+                Contador: { contador }
+            </Text>
+
+            <Fab 
+                title="+1"
+                onPress={ () => setContador( contador + 1 ) }
+            />
+
+            <Fab 
+                title="-1"
+                position="bl"
+                onPress={ () => setContador( contador - 1 ) }
+            />
+
+            {/* <TouchableOpacity
+                style={ styles.fabLocationBL }
+                onPress={ () => setContador( contador - 1 ) }
+            >
+                <View style={ styles.fab }>
+                    <Text style={ styles.fabText }> -1 </Text>
+                </View>
+            </TouchableOpacity>
+             */}
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 40,
+        top: -15,
+        textAlign: 'center',
+    },
+})
+
+export default ContadorScreens
